dddd5b8c9c01b45ba64ff1bf437b0a9eab4c6df2
Hyein Lee <70045318+ejin518@users.noreply.github.com>
2022-01-05 14:13:14 +1300

    Create index.php

---
+++ index.php
@@ -0,0 +1,449 @@
+<?php
+include_once('./common.php');
+include_once('./head_lib.php');
+include_once('./bbs/newwin.inc.php');
+?>
+
+<link rel="stylesheet" href="/css/common.css" type="text/css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css">
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/ycodetech/horizontal-timeline-2.0@2/css/horizontal_timeline.2.0.min.css">
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css"/>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js'></script>
+<script type="text/javascript" src="https://alvarotrigo.com/fullPage/fullpage.js"></script>
+<script type="text/javascript" src="/GnCommon/js/jquery.easing.min.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/ycodetech/horizontal-timeline-2.0@2/JavaScript/horizontal_timeline.2.0.min.js"></script>
+</head>
+<body>
+<div class="header">
+	<div class="head_top">
+		<div class="head_menu">
+			<div class="hamburger">
+				<span></span>
+			</div>
+		</div>
+		<div class="logo"><a href=""><img src="https://m.thingoolmarket.com/data/skin/mobile/moment_20210930_C/img/banner/5bceca8fd4a0d40fc89bfa2d6e037050_12902.png"></a></div>
+		<div class="head_my">
+			<span><img src="https://thingool.hgodo.com/data/mobile/moment_20210930_C/img/icon/icon_mypage.png"></span>
+			<span><img src="https://thingool.hgodo.com/data/mobile/moment_20210930_C/img/icon/icon_cart.png"></span>
+		</div>
+	</div>
+	<div class="head_gnb">
+		<ul class="clfix">
+			<li><a href="">띵굴의 설날</a></li>
+			<li><a href="">띵굴의 추천</a></li>
+			<li><a href="">베스트</a></li>
+			<li><a href="">알뜰살뜰</a></li>
+			<li><a href="">기획전</a></li>
+			<li><a href="">일생품</a></li>
+		</ul>
+	</div>
+	<div class="head_sub">
+		<em>설날 기획전_2022</em>
+	</div>
+</div>
+<div id="wrap">
+	<div class="content content01">
+		<em>BETTER DAY</em>
+		<div class="con01_tit01"><img src="https://m.thingoolmarket.com/data/event_2021/12/newyear_2022/pc/typo.png"></div>
+		<div class="con01_tit02">
+			<h1>범 내려온다</h1>
+			<h1>복 내려온다</h1>
+		</div>
+		<div class="con01_txt">
+			<p>새해 복 많~이 받으세요<br>
+			고마운 마음 가득 담아<br>
+			띵굴 호랑이가 준비했어요</p>
+		</div>
+		<span>22. 01. 03 - 2.2</span>
+	</div>
+	<div class="content content02">
+		<div class="con02_tit"></div>
+		<ul class="clfix">
+			<li>
+				<div><img src="https://m.thingoolmarket.com/data/event_2021/12/newyear_2022/mob/m_intro_01.png"></div>
+			</li>
+			<li>
+				<div><img src="https://m.thingoolmarket.com/data/event_2021/12/newyear_2022/mob/m_intro_02.png"></div>
+			</li>
+			<li>
+				<div><img src="https://m.thingoolmarket.com/data/event_2021/12/newyear_2022/mob/m_intro_03.png"></div>
+			</li>
+		</ul>
+	</div>
+	<div class="content content03">
+		<div class="gnb_menu" id="nav">
+			<ul>
+				<li class="active">
+					<button data-section="a">상차림</button>
+				</li>
+				<li>
+					<button data-section="b">선물세트</button>
+				</li>
+				<li>
+					<button data-section="c">이벤트</button>
+				</li>
+				<li>
+					<button data-section="d">배송 안내</button>
+				</li>
+			</ul>	
+		</div>
+
+		<section id="section01" data-section="a">
+			<div class="inner">
+				<div class="sec01_tit"></div>
+				<div class="sec01_sub">
+					<p>당신이 조금 더 편하길 바라요.<br>
+					띵굴 호랑이가 준비한 맛집 먹거리로<br>
+					신경 쓴 상차림을 간편히 준비하세요.</p>
+				</div>
+				<div class="sec01_img">
+					<img src="https://m.thingoolmarket.com/data/event_2021/12/newyear_2022/mob/m_e1_01.png">
+				</div>
+				<div class="sec01_slider">
+					<div class="sec01_slide">
+						<ul class="clfix">
+							<li>
+								<a href="">
+									<span><img src="https://m.thingoolmarket.com/data/event_2021/12/newyear_2022/mob/sang/1-6.png"></span>
+									<dl>
+										<dt>
+											<span>[한일관]</span>
+											<h2>꿀불고기</h2>
+										</dt>
+										<dd>
+											<em>36,000 > </em><strong>34,200</strong>
+										</dd>
+									</dl>
+								</a>
+							</li>
+							<li>
+								<a href="">
+									<span><img src="https://m.thingoolmarket.com/data/event_2021/12/newyear_2022/mob/sang/1-6.png"></span>
+									<dl>
+										<dt>
+											<span>[한일관]</span>
+											<h2>꿀불고기</h2>
+										</dt>
+										<dd>
+											<em>36,000 > </em><strong>34,200</strong>
+										</dd>
+									</dl>
+								</a>
+							</li>
+							<li>
+								<a href="">
+									<span><img src="https://m.thingoolmarket.com/data/event_2021/12/newyear_2022/mob/sang/1-6.png"></span>
+									<dl>
+										<dt>
+											<span>[한일관]</span>
+											<h2>꿀불고기</h2>
+										</dt>
+										<dd>
+											<em>36,000 > </em><strong>34,200</strong>
+										</dd>
+									</dl>
+								</a>
+							</li>
+							<li>
+								<a href="">
+									<span><img src="https://m.thingoolmarket.com/data/event_2021/12/newyear_2022/mob/sang/1-6.png"></span>
+									<dl>
+										<dt>
+											<span>[한일관]</span>
+											<h2>꿀불고기</h2>
+										</dt>
+										<dd>
+											<em>36,000 > </em><strong>34,200</strong>
+										</dd>
+									</dl>
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="sec01_slide">
+						<ul class="clfix">
+							<li>
+								<a href="">
+									<span><img src="https://m.thingoolmarket.com/data/event_2021/12/newyear_2022/mob/sang/1-6.png"></span>
+									<dl>
+										<dt>
+											<span>[한일관]</span>
+											<h2>꿀불고기</h2>
+										</dt>
+										<dd>
+											<em>36,000 > </em><strong>34,200</strong>
+										</dd>
+									</dl>
+								</a>
+							</li>
+							<li>
+								<a href="">
+									<span><img src="https://m.thingoolmarket.com/data/event_2021/12/newyear_2022/mob/sang/1-6.png"></span>
+									<dl>
+										<dt>
+											<span>[한일관]</span>
+											<h2>꿀불고기</h2>
+										</dt>
+										<dd>
+											<em>36,000 > </em><strong>34,200</strong>
+										</dd>
+									</dl>
+								</a>
+							</li>
+							<li>
+								<a href="">
+									<span><img src="https://m.thingoolmarket.com/data/event_2021/12/newyear_2022/mob/sang/1-6.png"></span>
+									<dl>
+										<dt>
+											<span>[한일관]</span>
+											<h2>꿀불고기</h2>
+										</dt>
+										<dd>
+											<em>36,000 > </em><strong>34,200</strong>
+										</dd>
+									</dl>
+								</a>
+							</li>
+							<li>
+								<a href="">
+									<span><img src="https://m.thingoolmarket.com/data/event_2021/12/newyear_2022/mob/sang/1-6.png"></span>
+									<dl>
+										<dt>
+											<span>[한일관]</span>
+											<h2>꿀불고기</h2>
+										</dt>
+										<dd>
+											<em>36,000 > </em><strong>34,200</strong>
+										</dd>
+									</dl>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="section02" data-section="b">
+			<div class="inner">
+				<div class="sec02_tit"></div>
+				<div class="sec01_sub">
+					<p>실속 있고 남다른 선물을 준비했어요.<br>
+					띵굴 호랑이 덕담 듣고<br>
+					명절 선물 준비해 보세요.</p>
+				</div>
+				<div class="sec01_img">
+					<img src="https://m.thingoolmarket.com/data/event_2021/12/newyear_2022/mob/m_e1_02.png">
+				</div>
+				<div class="sec01_slider">
+					<div class="sec01_slide">
+						<ul class="clfix">
+							<li>
+								<a href="">
+									<span><img src="https://m.thingoolmarket.com/data/event_2021/12/newyear_2022/mob/sang/1-6.png"></span>
+									<dl>
+										<dt>
+											<span>[한일관]</span>
+											<h2>꿀불고기</h2>
+										</dt>
+										<dd>
+											<em>36,000 > </em><strong>34,200</strong>
+										</dd>
+									</dl>
+								</a>
+							</li>
+							<li>
+								<a href="">
+									<span><img src="https://m.thingoolmarket.com/data/event_2021/12/newyear_2022/mob/sang/1-6.png"></span>
+									<dl>
+										<dt>
+											<span>[한일관]</span>
+											<h2>꿀불고기</h2>
+										</dt>
+										<dd>
+											<em>36,000 > </em><strong>34,200</strong>
+										</dd>
+									</dl>
+								</a>
+							</li>
+							<li>
+								<a href="">
+									<span><img src="https://m.thingoolmarket.com/data/event_2021/12/newyear_2022/mob/sang/1-6.png"></span>
+									<dl>
+										<dt>
+											<span>[한일관]</span>
+											<h2>꿀불고기</h2>
+										</dt>
+										<dd>
+											<em>36,000 > </em><strong>34,200</strong>
+										</dd>
+									</dl>
+								</a>
+							</li>
+							<li>
+								<a href="">
+									<span><img src="https://m.thingoolmarket.com/data/event_2021/12/newyear_2022/mob/sang/1-6.png"></span>
+									<dl>
+										<dt>
+											<span>[한일관]</span>
+											<h2>꿀불고기</h2>
+										</dt>
+										<dd>
+											<em>36,000 > </em><strong>34,200</strong>
+										</dd>
+									</dl>
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="sec01_slide">
+						<ul class="clfix">
+							<li>
+								<a href="">
+									<span><img src="https://m.thingoolmarket.com/data/event_2021/12/newyear_2022/mob/sang/1-6.png"></span>
+									<dl>
+										<dt>
+											<span>[한일관]</span>
+											<h2>꿀불고기</h2>
+										</dt>
+										<dd>
+											<em>36,000 > </em><strong>34,200</strong>
+										</dd>
+									</dl>
+								</a>
+							</li>
+							<li>
+								<a href="">
+									<span><img src="https://m.thingoolmarket.com/data/event_2021/12/newyear_2022/mob/sang/1-6.png"></span>
+									<dl>
+										<dt>
+											<span>[한일관]</span>
+											<h2>꿀불고기</h2>
+										</dt>
+										<dd>
+											<em>36,000 > </em><strong>34,200</strong>
+										</dd>
+									</dl>
+								</a>
+							</li>
+							<li>
+								<a href="">
+									<span><img src="https://m.thingoolmarket.com/data/event_2021/12/newyear_2022/mob/sang/1-6.png"></span>
+									<dl>
+										<dt>
+											<span>[한일관]</span>
+											<h2>꿀불고기</h2>
+										</dt>
+										<dd>
+											<em>36,000 > </em><strong>34,200</strong>
+										</dd>
+									</dl>
+								</a>
+							</li>
+							<li>
+								<a href="">
+									<span><img src="https://m.thingoolmarket.com/data/event_2021/12/newyear_2022/mob/sang/1-6.png"></span>
+									<dl>
+										<dt>
+											<span>[한일관]</span>
+											<h2>꿀불고기</h2>
+										</dt>
+										<dd>
+											<em>36,000 > </em><strong>34,200</strong>
+										</dd>
+									</dl>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="section03" data-section="c"></section>
+		<section id="section04" data-section="d"></section>
+	</div>
+	
+</div>
+	<div class="gnb">
+		<ul>
+			<li>	
+				<a href="">띵굴의 설날</a>
+				<ul class="gnb_inner">
+					<li><a href="">전체보기</a></li>
+					<li><a href="">설 상차림</a></li>
+					<li><a href="">설 선물</a></li>
+				</ul>
+			</li>
+			<li>	
+				<a href="">식료품</a>
+				<ul class="gnb_inner">
+					<li><a href="">전체보기</a></li>
+					<li><a href="">과일</a></li>
+					<li><a href="">채소</a></li>
+					<li><a href="">견과/양곡</a></li>
+					<li><a href="">전통주</a></li>
+					<li><a href="">정육/계랸</a></li>
+				</ul>
+			</li>
+			<li>	
+				<a href="">맛집먹거리</a>
+				<ul class="gnb_inner">
+					<li><a href="">전체보기</a></li>
+					<li><a href="">한식</a></li>
+					<li><a href="">중식</a></li>
+					<li><a href="">양식</a></li>
+				</ul>
+			</li>
+			<li>	
+				<a href="">띵굴의집</a>
+				<ul class="gnb_inner">
+					<li><a href="">전체보기</a></li>
+					<li><a href="">반가운 새해맞이</a></li>
+					<li><a href="">친환경 습관</a></li>
+				</ul>
+			</li>
+			<li>	
+				<a href="">코스트코</a>
+				<ul class="gnb_inner">
+					<li><a href="">전체보기</a></li>
+					<li><a href="">과일</a></li>
+					<li><a href="">채소</a></li>
+					<li><a href="">견과/양곡</a></li>
+					<li><a href="">전통주</a></li>
+					<li><a href="">정육/계랸</a></li>
+				</ul>
+			</li>
+			<li>	
+				<a href="">식료품</a>
+				<ul class="gnb_inner">
+					<li><a href="">1</a></li>
+					<li><a href="">2</a></li>
+					<li><a href="">3</a></li>
+				</ul>
+			</li>
+			<li>	
+				<a href="">식료품</a>
+				<ul class="gnb_inner">
+					<li><a href="">1</a></li>
+					<li><a href="">2</a></li>
+					<li><a href="">3</a></li>
+				</ul>
+			</li>
+			<li>	
+				<a href="">식료품</a>
+				<ul class="gnb_inner">
+					<li><a href="">1</a></li>
+					<li><a href="">2</a></li>
+					<li><a href="">3</a></li>
+				</ul>
+			</li>
+		</ul>
+		<div class="close">
+			<span></span>
+			<span></span>
+		</div>
+</
+
+</body>
+</html>
